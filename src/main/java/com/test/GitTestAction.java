fa124d4ac3abfe7928f8d2471e072b1c4d0c01b2
BarryChen123 <18201810187@163.com>
2017-09-25 15:21:11 +0800

    breanchOne分体提交代码：Hello World 123 B branchOne

--- src/main/java/com/test/GitTestAction.java
+++ src/main/java/com/test/GitTestAction.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class GitTestAction {
 	public static void main(String[] args) {
-		System.out.println("Hello World 123");
+		System.out.println("Hello World 123 B branchOne");
 	}
 }
