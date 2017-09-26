82eb6b9d9598866649bffcd163bbce6db736686a
BarryChen123 <18201810187@163.com>
2017-09-26 20:35:52 +0800

    添加代码CCC

--- src/main/java/com/test/GitTestAction.java
+++ src/main/java/com/test/GitTestAction.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class GitTestAction {
 	public static void main(String[] args) {
-		System.out.println("Hello World 123 B branchOne");
+		System.out.println("Hello World 123 B branchOne CCC");
 	}
 }
